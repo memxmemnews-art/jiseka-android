4fe43090045a171f46fa4ae454875a985691e1ea
memxmemnews-art <memxmemnews@gmail.com>
2026-05-27 17:52:56 +0900

    Create ImageProxyExtensions.kt

---
+++ app/src/main/java/com/jiseka/app/ImageProxyExtensions.kt
@@ -0,0 +1,12 @@
+package com.jiseka.app
+
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.ImageFormat
+import android.graphics.Matrix
+import android.graphics.Rect
+import android.graphics.YuvImage
+import androidx.camera.core.ImageProxy
+import java.io.ByteArrayOutputStream
+
+/**
