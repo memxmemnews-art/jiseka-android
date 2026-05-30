b88e0d783621b9a8c730e4f6128c573b7ad02e0c
memxmemnews-art <memxmemnews@gmail.com>
2026-05-30 09:22:36 +0900

    Update ImmutableModels.kt

--- app/src/main/java/com/jiseka/app/ImmutableModels.kt
+++ app/src/main/java/com/jiseka/app/ImmutableModels.kt
@@ -5,7 +5,7 @@ import android.graphics.RectF
 // 스레드 안전성을 보장하는 불변 포인트
 data class ImmutablePoint(val x: Float, val y: Float)
 
-// 1차 고속 필터링(바운딩 박스)과 2차 정밀 다각형을 묶어둔 앵커 데이터
+// 다각형의 점들과 바운딩 박스를 묶어둔 앵커 데이터 (점의 개수는 4~10개로 유동적)
 data class CandidatePolygon(
     val points: List<ImmutablePoint>,
     val bounds: RectF
